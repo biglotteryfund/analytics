ecf92b752817573587931ea0ed28595e9b474cbc
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-12-15 12:27:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Setup instructions
+
+First create a project as outlined here: https://github.com/debrouwere/google-analytics/wiki/Authentication
+
+Once your credentials are stored in your environment, run:
+
+    python3 -m venv ve
+    source ve/bin/activate
+    pip3 install googleanalytics
+    python ga.py
---
+++ ga.py
@@ -0,0 +1,26 @@
+import os
+import json
+import googleanalytics as ga
+
+if os.path.exists('credentials.json'):
+    credentials = json.load(open('credentials.json'))
+else:
+    credentials = ga.authorize()
+    credentials = credentials.serialize()
+    json.dump(credentials, open('credentials.json', 'w'))
+
+ga.authenticate(**credentials)
+
+profile = ga.authenticate(
+    save=True,
+    interactive=True,
+    identity='BLF Dev',
+    account='Big Lottery Fund',
+    webproperty='Biglotteryfund.org.uk'
+)
+
+pageviews = profile.core.query.metrics('pageviews').daily(months=-1).rows
+
+
+
+print(pageviews)
