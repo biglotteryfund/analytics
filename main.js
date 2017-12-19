98e06346e6d89e4b6c6d5b8a6ad2d3d6e870e228
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-12-19 11:16:16 +0000

    Merge in analysis

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // See: http://2ality.com/2015/10/google-analytics-api.html
 require('dotenv').config();
+const { head, orderBy } = require('lodash');
 const credentials = require('./credentials.json');
 const google = require('googleapis');
 const analytics = google.analytics('v3');
@@ -13,6 +14,50 @@ function queryData(query) {
       }
       resolve(response);
     });
+  });
+}
+
+function analyse({ queryRows, totalPageViews }) {
+  const cleanedRows = queryRows.map(row => {
+    const [fullUrl, pageviews] = row;
+
+    return {
+      fullUrl: fullUrl,
+      cleanUrl: fullUrl.indexOf('/~/link.aspx') !== -1 ? fullUrl : head(fullUrl.split('?')),
+      pageviews: parseInt(pageviews, 10)
+    }
+  });
+
+  const finalScores = cleanedRows.reduce((collection, currentRow) => {
+    const match = collection.find(row => row.cleanUrl === currentRow.cleanUrl);
+    if (match) {
+      match.pageviews = match.pageviews + currentRow.pageviews;
+    } else {
+      collection.push(currentRow);
+    }
+    return collection;
+  }, []);
+
+  const orderedFinalScores = orderBy(finalScores, ['pageviews'], ['desc']);
+
+  const targetPercentage = 80;
+  const pageviewsRequiredForTarget = totalPageViews / 100 * targetPercentage;
+
+  let count = 0;
+  const urlsToTarget = orderedFinalScores.filter(u => {
+      count += u.pageviews;
+      return count < pageviewsRequiredForTarget;
+  });
+
+  console.log(`
+    There are ${orderedFinalScores.length} unique URLs accessed in this period.
+    This covers ${totalPageViews} total pageviews.
+    If we want to replace ${targetPercentage}% of these pages with new ones, we need to replace ${urlsToTarget.length} of them
+  `);
+
+  urlsToTarget.map(row => {
+    const urlPath = row.cleanUrl.replace('www.biglotteryfund.org.uk', '');
+    console.log(`${urlPath} (${row.pageviews} pageviews)`)
   });
 }
 
@@ -35,11 +80,16 @@ jwtClient.authorize(function(err, tokens) {
     ids: VIEW_ID,
     metrics: 'ga:uniquePageviews',
     dimensions: 'ga:pagePath',
+    // 'start-date': '2017-12-11',
+    // 'end-date': '2017-12-17',
     'start-date': '30daysAgo',
     'end-date': 'yesterday',
-    sort: '-ga:uniquePageviews'
-    // filters: 'ga:pagePath!@.pdf'
+    sort: '-ga:uniquePageviews',
+    filters: 'ga:pagePath!@.pdf'
   }).then(data => {
-    console.log(JSON.stringify(data, null, 4));
+    analyse({
+      queryRows: data.rows,
+      totalPageViews: parseInt(data.totalsForAllResults['ga:uniquePageviews'], 10),
+    })
   }).catch(err => console.log(err));
 });
